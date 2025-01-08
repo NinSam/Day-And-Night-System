26770923f9de9e8c5ac6318575af8537bd3998f0
NinSam <ninsamgd@gmail.com>
2025-01-08 12:48:01 -0800

    ok

--- src/GJPromoPopup.cpp
+++ src/GJPromoPopup.cpp
@@ -1,15 +1,30 @@
 #include <Geode/Geode.hpp>
-#include <Geode/modify/SetTextPopup.hpp>
+#include <Geode/modify/GJPromoPopup.hpp>
 #include "Utils.hpp"
 
 using namespace geode::prelude;
 
-class $modify(SetTextPopup) {
+class $modify(GJPromoPopup) {
 
+	#ifdef GEODE_IS_MACOS_INTEL
+
+	bool init(gd::string p0) {
+		if (!GJPromoPopup::init(p0))
+		return false;
+
+
+	auto DayAndNightSystemOverlay = DayAndNightSystemOverlay::create();
+	DayAndNightSystemOverlay->setZOrder(106);
+	DayAndNightSystemOverlay->setID("ScreenOverlay"_spr);
+	this->addChild(DayAndNightSystemOverlay);
+
+	
+		return true;
+	}
+	#endif
 	#ifdef GEODE_IS_WINDOWS
-
-	bool init(gd::string p0, gd::string p1, int p2, gd::string p3, gd::string p4, bool p5, float p6) {
-		if (!SetTextPopup::init(p0, p1, p2, p3, p4, p5, p6))
+	bool init(gd::string p0) {
+		if (!GJPromoPopup::init(p0))
 		return false;
 
 
@@ -23,8 +38,8 @@ class $modify(SetTextPopup) {
 	}
 	#endif
 	#ifdef GEODE_IS_ANDROID
-	bool init(gd::string p0, gd::string p1, int p2, gd::string p3, gd::string p4, bool p5, float p6) {
-		if (!SetTextPopup::init(p0, p1, p2, p3, p4, p5, p6))
+	bool init(gd::string p0) {
+		if (!GJPromoPopup::init(p0))
 		return false;
 
 
