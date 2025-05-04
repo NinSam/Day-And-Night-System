26f671850b78ae1629f94533cd588ef838ec6ba2
NinSam <ninsamgd@gmail.com>
2025-05-04 02:24:51 -0700

    👍

--- src/GJPromoPopup.cpp
+++ src/GJPromoPopup.cpp
@@ -6,8 +6,6 @@ using namespace geode::prelude;
 
 class $modify(GJPromoPopup) {
 
-	#ifdef GEODE_IS_INTEL_MAC
-
 	bool init(gd::string p0) {
 		if (!GJPromoPopup::init(p0))
 		return false;
@@ -20,33 +18,4 @@ class $modify(GJPromoPopup) {
 	
 		return true;
 	}
-	#endif
-	#ifdef GEODE_IS_WINDOWS
-	bool init(gd::string p0) {
-		if (!GJPromoPopup::init(p0))
-		return false;
-
-
-	auto DayAndNightSystemOverlay = DayAndNightSystemOverlay::create();
-	DayAndNightSystemOverlay->setID("ScreenOverlay"_spr);
-	this->addChild(DayAndNightSystemOverlay,106);
-
-	
-		return true;
-	}
-	#endif
-	#ifdef GEODE_IS_ANDROID
-	bool init(gd::string p0) {
-		if (!GJPromoPopup::init(p0))
-		return false;
-
-
-	auto DayAndNightSystemOverlay = DayAndNightSystemOverlay::create();
-	DayAndNightSystemOverlay->setID("ScreenOverlay"_spr);
-	this->addChild(DayAndNightSystemOverlay,106);
-
-	
-		return true;
-	}
-	#endif
 };
